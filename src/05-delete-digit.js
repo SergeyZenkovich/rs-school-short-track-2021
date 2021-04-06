a5eb677ed9f067c8c8ee8ba1655ce4cf900d9bbb
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-04-07 00:41:47 +0300

    feat: add decision of task 5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,16 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const string = String(n);
+  let max = Number(string.slice(1));
+  for (let i = 1; i < string.length; i++) {
+    const tempMax = Number(string.slice(0, i) + string.slice(i + 1));
+    if (tempMax > max) {
+      max = tempMax;
+    }
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
