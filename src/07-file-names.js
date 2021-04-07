470598eb02d10d76b99b130181104ca6a0b2d730
unknown <sapocalipsys.z@gmail.com>
2021-04-07 18:04:33 +0300

    feat: add decision of task 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,23 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  if (names.length === 0) {
+    return names;
+  }
+  const tempObj = {};
+  const resultArr = [];
+  names.forEach((el) => {
+    if (el in tempObj) {
+      tempObj[el] += 1;
+      resultArr.push(`${el}(${tempObj[el]})`);
+      tempObj[`${el}(${tempObj[el]})`] = 0;
+    } else {
+      tempObj[el] = 0;
+      resultArr.push(el);
+    }
+  });
+  return resultArr;
 }
 
 module.exports = renameFiles;
