1a29afad470e48f6a2629dbf6ae810eea9aab503
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-04-09 00:27:13 +0300

    feat: add decision of task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,29 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const strArr = str.split('');
+  let tempStr = '';
+  let tempNum = 0;
+  let resultStr = '';
+
+  strArr.forEach((el, index) => {
+    if (el === tempStr) {
+      tempNum++;
+    } else if (tempStr !== '') {
+      resultStr += `${tempNum > 1 ? tempNum : ''}${tempStr}`;
+      tempStr = el;
+      tempNum = 1;
+    } else {
+      tempStr = el;
+      tempNum = 1;
+    }
+    if (index === strArr.length - 1) {
+      resultStr += `${tempNum > 1 ? tempNum : ''}${tempStr}`;
+    }
+  });
+
+  return resultStr;
 }
 
 module.exports = encodeLine;
